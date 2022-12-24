9161880c89a00c82a973db8e5ccd97639da3bd70
Pavel Tokarev <108064386+mcnight89@users.noreply.github.com>
2022-12-24 17:19:56 +0300

    Add files via upload

--- HW_23/app.py
+++ HW_23/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from views import main_bp
 
-app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run()
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(main_bp)
+    return app
